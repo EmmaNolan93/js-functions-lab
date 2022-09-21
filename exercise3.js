3ffb618bc5edd713b92fd1478dba1574802e8fa6
Emma Nolan <emmanolan2019@gmail.com>
2022-09-21 22:24:52 +0100

    Exercise 3.

--- exercise3.js
+++ exercise3.js
@@ -2,11 +2,20 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
      const completed = json// Complete this code
+     var todoTitles = [];
+     const myobject = {
+        userId:'Blank'
+     }
      completed.forEach( (todo, index) => {
-      if (todo.completed = true){
-      console.log(`${todo.title} - ${todo.completed}`)
+      if (todo.completed != true){
+     const myobjects = {
+        userId: todo.userId,
+        titles: todo.title
+     }
+     todoTitles.push(myobjects)
       }
     })
+    console.log(todoTitles);
   })
   .catch(function(err) { 
     console.log(err);
