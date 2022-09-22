9158aaa51c2c6b473844532196c217916052ea52
Emma Nolan <emmanolan2019@gmail.com>
2022-09-22 12:34:55 +0100

    Exercise 5.

---
+++ excercise5.js
@@ -0,0 +1,24 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const completed = json// Complete this code
+     var todoTitles = [];
+    /* completed.forEach( (todo, index) => {
+      if (todo.completed != true){
+     const myobject = {
+        userId: todo.userId,
+        titles: todo.title
+     }
+     todoTitles.push(myobject)
+      }
+    })*/
+ const a = completed.reduce((userId, titles, index, array)=> {
+    if(titles.completed = true){
+    return array = [ userId + "Title" + titles.titles + "UserId" +  titles.userId];
+    }
+})
+    console.log(todoTitles);
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
