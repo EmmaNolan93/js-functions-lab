9fc07eb452d8db7415c8b04d8dfb0221b99fdf7c
Emma Nolan <emmanolan2019@gmail.com>
2022-09-29 11:25:55 +0100

    Exercise 4.

--- excercuse4.js
+++ excercuse4.js
@@ -1,19 +1,11 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
-  .then(json => {
-     const completed = json// Complete this code
-     const array1 = [];
-     const initialValue = 0;
-     completed.forEach( (todo, index) => {
-      if (todo.completed = true){
-      array1.push(1)
-      }
-    })
-    const sumWithInitial = array1.reduce(
-        (previousValue, currentValue) => previousValue + currentValue,
-        initialValue
-      );
-      console.log(sumWithInitial)
+  .then(todos => {
+     const totalCompleted = todos.reduce( (acc, todo) => 
+          todo.completed ? acc+1 : acc
+     ,0
+     )
+     console.log(totalCompleted)
   })
   .catch(function(err) { 
     console.log(err);
